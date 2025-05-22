1a6a7103292a4cd814bf58d81db8ab281c599650
metallicat1 <20240029602@my.xu.edu.ph>
2025-05-22 22:56:24 +0800

    Initial commit

---
+++ src/main/java/com/pnrw/PNRW/PhilippineNationalRoadWatchApplication.java
@@ -0,0 +1,13 @@
+package com.pnrw.PNRW;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhilippineNationalRoadWatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhilippineNationalRoadWatchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pnrw/PNRW/PhilippineNationalRoadWatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pnrw.PNRW;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhilippineNationalRoadWatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
